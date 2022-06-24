43ac060ce252b9896100dd186b64fc7f1c33487c
thebooort <18428931+thebooort@users.noreply.github.com>
2022-06-24 10:04:39 +0200

    add init and toml

---
+++ recorpy/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+'''
+@File    :   __init__.py
+@Time    :   2022/06/24 09:59:42
+@Author  :   Bart Ortiz 
+@Version :   1.0
+@Contact :   bortiz@ugr.es
+@License :   CC-BY-SA or GPL3
+@Desc    :   Init file for the recorpy package
+'''
+
+def search(name, count=5):
+    """Search podcast by name."""
+    raise NotImplementedError()
